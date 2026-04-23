c267a564a528c62c2029adc88ed2b25359728446
Pixelsuft <pixelsuft@gmail.com>
2026-04-23 18:00:00 +0700

    wip

--- tools/timer_fix.cpp
+++ tools/timer_fix.cpp
@@ -24,13 +24,13 @@ ost::expected<void, std::string> timer_fix::save(std::vector<int>& data) {
             reinterpret_cast<uint8_t*>(eventPtr) + cfg.tm_fix_event_entry_offset;
         int type = *reinterpret_cast<int*>(currentEntry + cfg.tm_fix_event_entry_type_offset);
         if (type != -0xa0001 && type != -0x90001) {
-            for (int i = 0; i < totalEntries; ++i) {
+            for (int i = 0; i < totalEntries; i++) {
                 uint16_t entrySize = *reinterpret_cast<uint16_t*>(currentEntry);
                 int32_t objectID = *reinterpret_cast<int32_t*>(currentEntry + 2);
                 uint8_t paramCount = *reinterpret_cast<uint8_t*>(currentEntry + 0xC);
                 if (paramCount > 0) {
                     uint8_t* paramPtr = currentEntry + (objectID < 0 ? 0x10 : 0x0E);
-                    for (int p = 0; p < static_cast<int>(paramCount); ++p) {
+                    for (int p = 0; p < static_cast<int>(paramCount); p++) {
                         uint16_t pSize = *reinterpret_cast<uint16_t*>(paramPtr);
                         uint16_t pType = *reinterpret_cast<uint16_t*>(paramPtr + 2);
                         if (pType == 0x0D) {
@@ -38,7 +38,7 @@ ost::expected<void, std::string> timer_fix::save(std::vector<int>& data) {
                             int32_t timerValue = *reinterpret_cast<int32_t*>(paramPtr + 8);
                             data.push_back(intervalValue);
                             data.push_back(timerValue);
-                            spdlog::debug("saved {} / {}", timerValue, intervalValue);
+                            // spdlog::debug("saved {} / {}", timerValue, intervalValue);
                         }
                         paramPtr += pSize;
                     }
@@ -56,6 +56,53 @@ ost::expected<void, std::string> timer_fix::load(std::vector<int> data) {
     auto& cfg = conf::get();
     if (!cfg.allow_timers_fix)
         return {};
+    if (data.empty()) {
+        spdlog::warn("Not fixing timers");
+        return {};
+    }
     ASS(data.size() % 2 == 0);
+    auto it = data.begin();
+    size_t eventPtr = *reinterpret_cast<size_t*>(reinterpret_cast<size_t>(cfg.gStats) + 0x80);
+    if (eventPtr == 0) {
+        spdlog::error("eventPtr is nullptr WTF");
+        return ost::unexpected<std::string>("failed to save timers - eventPtr was nullptr");
+    }
+    while (*reinterpret_cast<short*>(eventPtr) != 0) {
+        uint8_t nEvents = *reinterpret_cast<uint8_t*>(eventPtr + 2);
+        uint8_t nConds = *reinterpret_cast<uint8_t*>(eventPtr + 3);
+        int totalEntries = static_cast<int>(nConds) + static_cast<int>(nEvents);
+        uint8_t* currentEntry =
+            reinterpret_cast<uint8_t*>(eventPtr) + cfg.tm_fix_event_entry_offset;
+        int type = *reinterpret_cast<int*>(currentEntry + cfg.tm_fix_event_entry_type_offset);
+        if (type != -0xa0001 && type != -0x90001) {
+            for (int i = 0; i < totalEntries; i++) {
+                uint16_t entrySize = *reinterpret_cast<uint16_t*>(currentEntry);
+                int32_t objectID = *reinterpret_cast<int32_t*>(currentEntry + 2);
+                uint8_t paramCount = *reinterpret_cast<uint8_t*>(currentEntry + 0xC);
+                if (paramCount > 0) {
+                    uint8_t* paramPtr = currentEntry + (objectID < 0 ? 0x10 : 0x0E);
+                    for (int p = 0; p < static_cast<int>(paramCount); p++) {
+                        uint16_t pSize = *reinterpret_cast<uint16_t*>(paramPtr);
+                        uint16_t pType = *reinterpret_cast<uint16_t*>(paramPtr + 2);
+                        if (pType == 0x0D) {
+                            int32_t intervalValue = *reinterpret_cast<int32_t*>(paramPtr + 4);
+                            int32_t* timerValue = reinterpret_cast<int32_t*>(paramPtr + 8);
+                            if (it == data.end())
+                                return ost::unexpected<std::string>(
+                                    "WTF not enough data to fix timers");
+                            if (intervalValue != *it)
+                                return ost::unexpected<std::string>("Fixing timers gone wrong");
+                            it++;
+                            *timerValue = *it;
+                            it++;
+                        }
+                        paramPtr += pSize;
+                    }
+                }
+                currentEntry += entrySize;
+            }
+        }
+        eventPtr -= static_cast<size_t>(*reinterpret_cast<short*>(eventPtr));
+    }
     return {};
 }
