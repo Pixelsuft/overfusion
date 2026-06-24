6f0c889a2e7ca555756ca3a7931ebbe12e075943
Pixelsuft <pixelsuft@gmail.com>
2026-06-24 16:43:57 +0700

    wip vs2015

--- scripts/build_injector.bat
+++ scripts/build_injector.bat
@@ -1,6 +1,6 @@
 @echo off
 cl.exe /nologo /O2 /GS- /W3 /TC /c ofinjector.c
-link.exe /nologo /NODEFAULTLIB /ENTRY:WinMainCRTStartup /SUBSYSTEM:CONSOLE ^
+link.exe /nologo /NODEFAULTLIB /ENTRY:WinMainCRTStartup /SUBSYSTEM:WINDOWS,5.01 ^
          kernel32.lib shell32.lib user32.lib ofinjector.obj /OUT:ofinjector.exe
 del ofinjector.obj
 echo Done!
